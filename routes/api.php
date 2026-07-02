c09f27a8403567e6fb4b16c0dd103ff7882de802
Baicu Stefan <stefan.baicu@stud.ubbcluj.ro>
2026-07-02 11:25:54 +0300

    b

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,6 @@
 use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/recipes/random', [RecipeController::class, 'random']);
+Route::get('recipes/random', [RecipeController::class, 'random'])->name('recipes.random');
+
 Route::apiResource('recipes', RecipeController::class);
