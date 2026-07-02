04afb0e32639430df7f66ea142fdcb8efb0b3d12
Baicu Stefan <stefan.baicu@stud.ubbcluj.ro>
2026-07-02 14:11:49 +0300

    a

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -2,17 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'cook_time',
         'ingredients',
         'instructions'
     ];
-
+    
     protected $casts = [
         'ingredients' => 'array',
     ];
