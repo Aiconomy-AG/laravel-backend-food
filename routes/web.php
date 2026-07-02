59ebf93b31965ace0e8e923a8e407415b30cc237
Baicu Stefan <stefan.baicu@stud.ubbcluj.ro>
2026-07-02 15:38:51 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,3 @@
 use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/recipes/random', [RecipeController::class, 'random']);
-
-Route::apiResource('recipes', RecipeController::class);
